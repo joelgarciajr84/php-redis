3167bfe8e01a7af5b120040a5d469d1b29201746
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-01-13 18:16:00 -0200

    Update string.php

--- string.php
+++ string.php
@@ -12,12 +12,12 @@ echo "<h2>Script Running now: </h2>";
 highlight_file("string.php");
 ?>
 <ul>
-
-	<li><a href="strings.php">Strings</a></li>
-	<li><a href="lists.php">Lists</a></li>
-	<li><a href="sets.php">Sets</a></li>
-	<li><a href="sorted-sets.php">Sorted Sets</a></li>
-	<li><a href="hashes.php">Hashes</a></li>
-	<li><a href="keys.php">Keys</a></li>
+  <li><a href="index.php">Index</a></li>
+  <li><a href="string.php">Strings</a></li>
+  <li><a href="list.php">Lists</a></li>
+  <li><a href="sets.php">Sets</a></li>
+  <li><a href="sorted-sets.php">Sorted Sets</a></li>
+  <li><a href="hashes.php">Hashes</a></li>
+  <li><a href="keys.php">Keys</a></li>
 
 </ul>
