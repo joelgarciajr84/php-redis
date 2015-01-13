9362067d32852bf420cced929711cb683dd33e9c
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-01-13 18:14:48 -0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ echo "<h1>Redis Simple Examples</h1>";
 
 ?>
 <ul>
-
-	<li><a href="hashes.php">Strings</a></li>
-	<li><a href="hashes.php">Lists</a></li>
-	<li><a href="hashes.php">Sets</a></li>
-	<li><a href="hashes.php">Sorted Sets</a></li>
-	<li><a href="hashes.php">Hashes</a></li>
-	<li><a href="hashes.php">Keys</a></li>
+  <li><a href="index.php">Index</a></li>
+  <li><a href="string.php">Strings</a></li>
+  <li><a href="list.php">Lists</a></li>
+  <li><a href="sets.php">Sets</a></li>
+  <li><a href="sorted-sets.php">Sorted Sets</a></li>
+  <li><a href="hashes.php">Hashes</a></li>
+  <li><a href="keys.php">Keys</a></li>
 
 </ul>
