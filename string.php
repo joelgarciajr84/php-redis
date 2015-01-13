5ef0d0ad9f613d7d33e4d67e7c3acc56dc8afe72
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-01-13 18:11:25 -0200

    Update string.php

--- string.php
+++ string.php
@@ -13,11 +13,11 @@ highlight_file("string.php");
 ?>
 <ul>
 
-	<li><a href="hashes.php">Strings</a></li>
-	<li><a href="hashes.php">Lists</a></li>
-	<li><a href="hashes.php">Sets</a></li>
-	<li><a href="hashes.php">Sorted Sets</a></li>
+	<li><a href="strings.php">Strings</a></li>
+	<li><a href="lists.php">Lists</a></li>
+	<li><a href="sets.php">Sets</a></li>
+	<li><a href="sorted-sets.php">Sorted Sets</a></li>
 	<li><a href="hashes.php">Hashes</a></li>
-	<li><a href="hashes.php">Keys</a></li>
+	<li><a href="keys.php">Keys</a></li>
 
 </ul>
