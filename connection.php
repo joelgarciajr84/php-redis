0a1cf141c2f0b8fa16a2f3f30691d3b18c52b410
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-01-13 18:14:19 -0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -13,12 +13,3 @@ if (!$RedisJoel->connect('127.0.0.1', 6379)) {
 echo "<h2>Script Running now: </h2>";
 //highlight_file("connection.php");
 ?>
-<ul>
-	<li><a href="hashes.php">Strings</a></li>
-	<li><a href="hashes.php">Lists</a></li>
-	<li><a href="hashes.php">Sets</a></li>
-	<li><a href="hashes.php">Sorted Sets</a></li>
-	<li><a href="hashes.php">Hashes</a></li>
-	<li><a href="hashes.php">Keys</a></li>
-
-</ul>
